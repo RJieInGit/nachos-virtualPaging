0e3d216ead3dafb5e60d4bbceeb5e6b0fb4ff431
任杰 <renjie@ninmasarus-MacBook-puro.local>
2019-11-11 17:49:40 -0500

    db

---
+++ code/userprog/ksyscall.h
@@ -0,0 +1,91 @@
+/**************************************************************
+ *
+ * userprog/ksyscall.h
+ *
+ * Kernel interface for systemcalls 
+ *
+ * by Marcus Voelp  (c) Universitaet Karlsruhe
+ *
+ **************************************************************/
+
+#ifndef __USERPROG_KSYSCALL_H__ 
+#define __USERPROG_KSYSCALL_H__ 
+
+#include "kernel.h"
+
+
+
+
+void SysHalt()
+{
+  kernel->interrupt->Halt();
+}
+
+
+int SysAdd(int op1, int op2)
+{
+  return op1 + op2;
+}
+
+void SysWrite(int buffer, int size){
+  int content;
+  for(int i=0;i<size;i++){
+    kernel->machine->ReadMem(buffer,1,&content);
+    buffer++;
+    printf("%c",content);
+  }
+}
+
+void SysRead(int buffer,int size, char* content){
+  int c;
+  for(int i=0;i>size;i++){
+    c=(int)content[i];
+    kernel->machine->ReadMem(buffer,1,&c);
+  }
+}
+
+void SysExit(){
+  delete kernel->currentThread->space;
+  kernel->currentThread->Finish();
+}
+
+void usrFork(int arg){
+  kernel->currentThread->RestoreUserState();
+  kernel->machine->Run();
+}
+int SysFork(){
+  int funcaddr= kernel->machine->ReadRegister(4);
+   Thread *t = new Thread("SC_forked thread");
+   t->space = new AddrSpace(kernel->currentThread->space);
+   t->SaveUserState();
+   t->setReg(PCReg,funcaddr);
+   t->setReg(NextPCReg,funcaddr+4);
+   t->Fork((VoidFunctionPtr)usrFork,(void *)0);
+
+    return t->space->spaceId;
+}
+
+void execPro(AddrSpace *space){
+  ASSERT(space != (AddrSpace *)NULL);
+  space->Execute();
+}
+
+int  SysExec(int filename){
+  int size=100;
+  int addr = filename;
+  char* name = new char[100];
+  for(int i=0;i<size;i++){
+    int c;
+    if(!kernel->machine->ReadMem(addr,size,&c))
+      break;
+      name[i]=c;
+  }
+   AddrSpace *space = new AddrSpace;
+   space->Load(name);
+   Thread *th = new Thread("execthread");
+   th->Fork((VoidFunctionPtr)execPro,(void *)space); 
+   return 0;
+}
+
+
+#endif /* ! __USERPROG_KSYSCALL_H__ */
