081f877752b1bc9df1d4dc3aa700e3eefbc93857
任杰 <renjie@ninmasarus-MacBook-puro.local>
2019-11-13 21:31:47 -0500

    lru

--- code/userprog/ksyscall.h
+++ code/userprog/ksyscall.h
@@ -88,7 +88,7 @@ int  SysExec(int filename){
   int addr = filename;
   char* name = new char[100];
   for(int i=0;i<size;i++){
-    char c;
+    int c;
     if(!kernel->machine->ReadMem(addr,1,&c))
       break;
       name[i]=c;
